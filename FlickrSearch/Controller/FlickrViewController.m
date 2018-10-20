7da76c213bc87dc158e0648eec7a8af0b36aa679
abhishek-kharb <abhishekkh@hike.in>
2018-10-20 17:52:25 +0530

    Add search bar functionality to view controller

--- FlickrSearch/Controller/FlickrViewController.m
+++ FlickrSearch/Controller/FlickrViewController.m
@@ -8,9 +8,13 @@
 
 #import "FlickrViewController.h"
 
-@interface FlickrViewController () <FlickrDataSourceDelegateProtocol>
+@interface FlickrViewController () <FlickrDataSourceDelegateProtocol, UISearchBarDelegate>
 @property (nonatomic)id<FlickrDataSourceProtocol> dataSource;
+@property (nonatomic) UISearchBar *searchBar;
+@property (nonatomic) NSString *searchString;
 @end
+
+static CGFloat kSearchBarHeightConstant = 50.0f;
 
 @implementation FlickrViewController
 
@@ -26,8 +30,41 @@
 #pragma mark - View Methods
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setTitle:@"Photo Search"];
+    [self setupSearchBar];
 }
+
+#pragma mark - Action Methods
+- (void)setupSearchBar {
+    //We calculate these programatically as statusbar height varies for iPhoneX and below models.
+    CGFloat statusBarHeight = [UIApplication sharedApplication].statusBarFrame.size.height;
+    CGFloat navBarHeight = self.navigationController.navigationBar.bounds.size.height;
+    CGFloat topConstraintPadding =  statusBarHeight + navBarHeight;
+    
+    self.searchBar = [[UISearchBar alloc] init];
+    [self.view addSubview:self.searchBar];
+    [self.searchBar setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self.searchBar.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:topConstraintPadding].active = YES;
+    [self.searchBar.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor].active = YES;
+    [self.searchBar.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor].active = YES;
+    [self.searchBar.heightAnchor constraintEqualToConstant:kSearchBarHeightConstant].active = YES;
+    [self.searchBar setSearchBarStyle:UISearchBarStyleDefault];
+    [self.searchBar setShowsCancelButton:YES];
+    [self.searchBar setDelegate:self];
+    [self.searchBar setPlaceholder:@"Enter text and press search..."];
+}
+
+#pragma mark - UISearchBarDelegate Methods
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    self.searchString = searchBar.text;
+    [searchBar resignFirstResponder];
+    [self.dataSource fetchResultsWithSearchString:self.searchString];
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    [searchBar resignFirstResponder];
+}
+
 
 #pragma mark FlickrDataSourceDelegateProtocol Methods
 - (void)fetchedDataAvailable {
