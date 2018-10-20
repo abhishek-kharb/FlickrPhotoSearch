cabf004c3bb76737f987d0cce5c654ce0dec6285
abhishek-kharb <abhishekkh@hike.in>
2018-10-20 13:52:24 +0530

    Initial Commit

---
+++ FlickrSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrSearch
+//
+//  Created by Abhishek Kharb on 20/10/18.
+//  Copyright © 2018 Abhishek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrSearch/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FlickrSearch
+//
+//  Created by Abhishek Kharb on 20/10/18.
+//  Copyright © 2018 Abhishek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
